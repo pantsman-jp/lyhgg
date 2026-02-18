6a167018fc553a532d3ff23b46fb94ffa12c84c5
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-02-18 14:11:11 +0900

    initial commit and section1

---
+++ baby.hs
@@ -0,0 +1,15 @@
+doubleMe x = x + x
+
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber x = if x > 100 then x else x * 2
+
+doubleSmallNumber' x = doubleSmallNumber x + 1
+
+conanO'Brien = "It's a-me, Conan O'Brien"
+
+boomBangs xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+
+length' xs = sum [1 | _ <- xs]
+
+removeNonUppercase s = [c | c <- s, c `elem` ['A' .. 'Z']]

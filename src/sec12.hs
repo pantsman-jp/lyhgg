f2513c92dd19a214ba833dec370f3bdc2e4b7acb
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-03-04 14:24:50 +0900

    ~12.3

--- src/sec12.hs
+++ src/sec12.hs
@@ -1,3 +1,6 @@
+import Data.Monoid
+
+-- 12.1
 newtype Pair b a = Pair {getPair :: (a, b)}
 
 instance Functor (Pair c) where
@@ -7,3 +10,9 @@ newtype CoolBool = CoolBool {getCoolBool :: Bool}
 
 helloMe :: CoolBool -> String
 helloMe (CoolBool _) = "hello"
+
+-- 12.3
+lengthCompare :: String -> String -> Ordering
+lengthCompare x y = (length x `compare` length y) `mappend` (vowels x `compare` vowels y) `mappend` (x `compare` y)
+  where
+    vowels = length . filter (`elem` "aeiou")

5b1491151c1c962091dd1acfcf857541843ebba6
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-03-04 13:50:04 +0900

    ~12.1

---
+++ src/sec12.hs
@@ -0,0 +1,9 @@
+newtype Pair b a = Pair {getPair :: (a, b)}
+
+instance Functor (Pair c) where
+  fmap f (Pair (x, y)) = Pair (f x, y)
+
+newtype CoolBool = CoolBool {getCoolBool :: Bool}
+
+helloMe :: CoolBool -> String
+helloMe (CoolBool _) = "hello"
